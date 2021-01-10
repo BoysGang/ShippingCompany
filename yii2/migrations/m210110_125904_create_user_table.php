0c4d5054d066c7fd148d456fb6f32e3fc0a2e5df
Evster <8961240@gmail.com>
2021-01-10 20:15:28 +0700

    add registration

---
+++ yii2/migrations/m210110_125904_create_user_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+class m210110_125904_create_user_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable('user', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->notNull()->unique(),
+            'auth_key' => $this->string(32)->notNull(),
+            'password_hash' => $this->string()->notNull(),
+            'password_reset_token' => $this->string()->unique(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], null);
+    }
+
+    public function down()
+    {
+        $this->dropTable('user');
+    }
+}
