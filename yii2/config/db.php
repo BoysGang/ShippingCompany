8a424c9eb58f80bf26d47e24ebdc4aa21bf913e7
Evster <8961240@gmail.com>
2021-01-10 19:42:11 +0700

    try yii2

---
+++ yii2/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=94.251.36.104;port=5432;dbname=ShippingCompany',
+    'username' => 'postgres',
+    'password' => 'qwerty12345',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
