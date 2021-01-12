3bd58f473619d3fdab0b2a3431837206882ff548
a1pha1337 <45879854+a1pha1337@users.noreply.github.com>
2021-01-12 21:11:51 +0700

    Schedule view has been added

---
+++ yii2/views/dispatcher/schedule.php
@@ -0,0 +1,49 @@
+<?php
+	use yii\bootstrap\NavBar;
+	use yii\helpers\Html;
+	use yii\helpers\Url;
+	use yii\bootstrap\Nav;
+	use yii\grid\GridView;
+
+?>
+<div class="horizontalMenu">
+	<ul class="nav nav-tabs">
+	  <li class="nav-item">
+		<?= Html::a('Заявки', ['dispatcher/requests'], ['class' => 'btn-secondary btn-lg']) ?>
+	  </li>
+	  <li class="nav-item">
+		<?= Html::a('Расписание', ['dispatcher/schedule'], ['class' => 'btn-secondary btn-lg']) ?>
+	  </li>
+	</ul>
+</div>
+
+<h1>Расписание рейсов:</h1>
+    </script>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'PK_Trip',
+            'FirstPort',
+            'LastPort',
+            'UnitPriceRubles',
+            'DateDeparture',
+            'DateArrival',
+            [
+             'attribute' => 'PK_Ship',
+             'value' => 'ShipName'
+            ],
+
+            ['class' => 'yii\grid\ActionColumn',
+            'template' => '{view}',
+            'buttons' =>[
+            	'view' => function ($url, $model)
+            	{
+            		$url = Url::to(['client/viewtrip', 'id' => $model->PK_Trip]);
+            		return Html::a('Подробнее..', $url, ['title' => 'view']);
+            	}
+            ]],
+        ],
+    ]);
+    ?>
