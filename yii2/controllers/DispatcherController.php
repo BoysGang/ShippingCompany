1ab3e2b2dad6472bc8b0f52dff2c6819e9e89c97
a1pha1337 <45879854+a1pha1337@users.noreply.github.com>
2021-01-12 21:09:20 +0700

    Requests and Schedule render have been added

--- yii2/controllers/DispatcherController.php
+++ yii2/controllers/DispatcherController.php
@@ -8,11 +8,49 @@ use yii\web\Controller;
 use yii\web\Response;
 use yii\data\ActiveDataProvider;
 
+use app\models\Request;
+use app\models\Client;
+use app\models\Trip;
+
 class DispatcherController extends Controller
 {
-	//action render page Index
-	public function actionIndex()
+	public function console_log($data)
 	{
-		return $this->render('index');
+		echo "<script>";
+		echo "console.log(" . json_encode($data) . ")";
+		echo "</script>";
+	}
+
+	public function actionRequests()
+	{
+		$query = Request::find()->where(['RequestStatus' => 'verifying']);
+
+		$dataProvider = new ActiveDataProvider([
+		    'query' => $query,
+	    	'pagination' => [
+	        'pageSize' => 20,
+	    	],
+		]);
+
+		return $this->render('requests', [
+			'dataProvider' => $dataProvider,
+		]);
+	}
+
+	public function actionSchedule()
+	{
+		$query = Trip::find()->select('Trip.*, Route.*')
+			->leftJoin('Route', '"Route"."PK_Trip" = "Trip"."PK_Trip"')
+			->where(['>', 'DateDeparture', date("Y-m-d H:i:s")]);
+		$dataProvider = new ActiveDataProvider([
+		    'query' => $query,
+	    	'pagination' => [
+	        'pageSize' => 20,
+	    	],
+		]);
+
+		return $this->render('schedule', [
+			'dataProvider' => $dataProvider,
+		]);
 	}
 }
